eede52e496661829e80a8d599fe1638c7fb67f9f
suyufeng <2533677539@qq.com>
2016-07-24 17:52:25 +0800

    new

--- acm/2016-07-24(2016 Multi-University Training Contest 2 )/E.cpp
+++ acm/2016-07-24(2016 Multi-University Training Contest 2 )/E.cpp
@@ -1,103 +1,132 @@
 #include<cstdio>
-#include<map>
 #include<iostream>
 #include<algorithm>
+#include<cmath>
 using namespace std;
-const int maxn=1111;
+const int maxn=1010;
+const int mo=1e9+7;
 typedef long long LL;
-int n;
+LL gcd(LL a,LL b){
+	if(!b)return a;
+	return gcd(b,a%b);
+}
 struct P{
-	int x,y,cnt;
+	LL x,y,cnt;
 	P(){}
-	P(int _x,int _y){
+	P(LL _x,LL _y){
 		x=_x;y=_y;
 	}
-}p[maxn];
+	void read(){
+		int tx,ty;
+		scanf("%d%d",&tx,&ty);
+		x=tx;y=ty;
+	}
+}p[maxn],tp[maxn];
 bool operator<(const P &a,const P &b){
 	return a.x!=b.x?a.x<b.x:a.y<b.y;
 }
 bool operator==(const P &a,const P &b){
 	return a.x==b.x&&a.y==b.y;
 }
-map<pair<int,int>,int>M;
-map<pair<int,int>,int>M2;
-P ang[maxn];
-int gcd(int a,int b){
-	if(!b)return a;
-	return gcd(b,a%b);
+P operator+(const P &a,const P &b){
+	return P(a.x+b.x,a.y+b.y);
 }
-const int mo=1e9+7;
-long long pw(long long x,long long k,long long mo){
-	long long ans=1;
-	for(;k;k>>=1){
-		if(k&1)ans=ans*x%mo;
-		x=x*x%mo;
-	}return ans;
+P operator-(const P &a,const P &b){
+	return P(a.x-b.x,a.y-b.y);
 }
-LL _inv(int x){
-	return pw(x,mo-2,mo);
-}
-LL inv[maxn],two[maxn];
+int n;
+long long two[maxn];
+int siz,siz2;
+P ang[maxn],tang[maxn];
+long long cot[maxn];
 int main(){
-	
 	two[0]=1;
-	for(int i=1;i<maxn;i++)inv[i]=_inv(i);
-	for(int i=1;i<maxn;i++)two[i]=two[i-1]*2%mo;
-
-//	for(int i=1;i<=5;i++)
-//		cout<<inv[i]<<" ";
-
-	int T;scanf("%d",&T);	
+	for(int i=1;i<maxn;i++)
+		two[i]=(two[i-1]+two[i-1])%mo;
+	int T;
+	scanf("%d",&T);
 	while(T--){
 		long long ans=0;
 		scanf("%d",&n);
-		M.clear();
 		for(int i=1;i<=n;i++){
-			scanf("%d%d",&p[i].x,&p[i].y);
-			M[make_pair(p[i].x,p[i].y)]++;
+			p[i].read();
+			tp[i]=p[i];
 		}
-		sort(p+1,p+1+n);
-		n=unique(p+1,p+1+n)-p-1;
-		for(int i=1;i<=n;i++)
-			p[i].cnt=M[make_pair(p[i].x,p[i].y)];
-			
-		if(n==1){
-			ans=((LL)two[p[1].cnt]-p[1].cnt-1+mo+mo)%mo;
-			cout<<ans<<endl;
-			continue;
+		sort(tp+1,tp+1+n);
+		
+		
+		
+		int nn=unique(tp+1,tp+1+n)-tp-1;
+		
+		for(int i=1;i<=nn;i++){
+			cot[i]=0;
+			for(int j=1;j<=n;j++){
+				if(tp[i]==p[j])	
+					cot[i]++;
+			}
 		}
 		
-		//cerr<<n<<endl;
-		//for(int i=1;i<=n;i++)
-		//	printf("%d%c",p[i].cnt," \n"[i==n]);
+		if(nn==1){
+			ans=(two[n]-n-1+mo)%mo;
+			cout<<(ans+mo)%mo<<endl;
+			continue;
+		}	
+		
+		for(int i=1;i<=nn;i++)
+			ans=(ans+two[cot[i]]-cot[i]-1+mo)%mo;
+//		cerr<<ans<<endl;
+		
+		for(int i=1;i<=nn;i++){
+			siz=0,siz2=0;
+			int self=cot[i];
 			
-		for(int i=1;i<=n;i++){
-			int siz=0;
-			for(int j=1;j<=n;j++)if(i!=j){
-				ang[++siz]=P(p[j].x-p[i].x,p[j].y-p[i].y);
-				int d=gcd(ang[siz].x,ang[siz].y);
-				d=abs(d);
-				ang[siz].x/=d;
-				ang[siz].y/=d;				
-				if(ang[siz].x<0)ang[siz].x*=-1,ang[siz].y*=-1;
-				if(ang[siz].x==0)ang[siz].y=1;
+			
+			for(int j=1;j<=nn;j++)if(i!=j){
+				siz++;
+				ang[siz]=P(tp[j].x-tp[i].x,tp[j].y-tp[i].y);
+				LL d=gcd(abs(ang[siz].x),abs(ang[siz].y));
+				if(d != 1) {
+					ang[siz].x/=d;ang[siz].y/=d;
+				}
+				ang[siz].cnt=cot[j];
 			}
 			
 			
-			sort(ang+1,ang+1+siz);	
-//			printf("%d\n",siz);
-//			for(int j=1;j<=siz;j++)
-//				printf("%d %d %c",ang[j].x,ang[j].y," \n"[j==siz]);
-			
-			M.clear();
-			for(int i=1;i<=siz;i++)
-				M[make_pair(ang[i].x,ang[i].y)]+=ang[i].cnt;
-			for()
-			
-			
+			sort(ang+1,ang+1+siz);
+			for(int j=1;j<=siz;j++)
+				tang[++siz2]=ang[j];
+				
+			siz2=unique(tang+1,tang+1+siz2)-tang-1;
+				
+			LL cnt=self;
+			//cerr<<"self"<<" "<<self<<endl;
+			two[0]=0;
+			long long res=(long long)(two[self]-self-1+mo)%mo;
+			for(int j=1;j<=siz;j++){
+				cnt+=ang[j].cnt;
+				if(ang[j].cnt>=2)
+				res+=(long long)(two[ang[j].cnt]-ang[j].cnt-1+mo)%mo;
+				res%=mo;
+				
+				if(j==siz||!(ang[j]==ang[j+1])){
+					//cerr<<cnt<<" "<<(sgn(ang[j])>0)<<" "<<check(ang[j])<<endl;
+					if(cnt>=2&&(ang[j].x>0||(ang[j].x==0&&ang[j].y<0))&&!binary_search(tang+1,tang+1+siz2,P(-ang[j].x,-ang[j].y))){
+						ans=(ans+(long long)two[cnt]-cnt-1+mo);
+						ans+=mo;
+						ans=(ans-res+mo)%mo;
+						ans %= mo;
+						
+						//cerr<<"res"<<" "<<res<<endl;
+						//cerr<<"cnt"<<" "<<cnt<<endl;
+					}
+					res=(long long)(two[self]-self-1+mo)%mo;
+					cnt=self;
+				}
+			}
 		}
-		cout<<ans%mo<<endl;
-		
+		cout<<(ans+mo)%mo<<endl;
 	}
 	return 0;
 }
+
+
